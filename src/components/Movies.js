8926ae7ef5be8e51bd592e2a94a1797b225fa6e6
DavidRMorphew <morphewd@umich.edu>
2021-06-09 15:47:57 -0400

    commit tree 1beb06c2dec27dd8eae131140e13181d39d3e91a

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  
+  const renderMovies = () => (
+    movies.map(movie => (
+      <div key={movie.title}>
+        <h3>{movie.title}</h3>
+        <p>Duration: {movie.time} minutes</p>
+        <p>Genres: {movie.genres.join(", ")}</p>
+      </div>
+    ))
+  )
+  
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
